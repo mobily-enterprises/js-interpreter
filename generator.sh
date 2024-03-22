c5b4a64ecc836a8b7fcb9432b9f92f86e9a4f4a5
Tony Mobily <tonymobily@gmail.com>
2024-03-22 21:16:13 +0100

    Generator fixes

--- generator.sh
+++ generator.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # STEP 1: Copy Acorn over
-cp js-interpreter/acorn.js .
+cp JS-Interpreter/acorn.js .
 
 # STEP 2: Make up the interpreter
 # This will just simply prepend the definition of globalThis
 echo "var globalThis = exports" > js-interpreter.js
 echo "globalThis.acorn = require('./acorn.js')" >> js-interpreter.js
-cat js-interpreter/interpreter.js >> js-interpreter.js
+cat JS-Interpreter/interpreter.js >> js-interpreter.js
 
